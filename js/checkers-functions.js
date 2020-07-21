07ce641b38f0061c49e1605aa5a5f940bc624d89
LaurynasAnd <67257508+LaurynasAnd@users.noreply.github.com>
2020-07-21 12:14:12 +0300

    CSS style update

--- js/checkers-functions.js
+++ js/checkers-functions.js
@@ -10,13 +10,24 @@ function renderBoard(data) {
         //generuojame eilutes celes
         for (let j = 0; j < data.width; j++) {
             
-            cellHTML += `<div class="cell ${((i + j) % 2 === 0) ? 'white' : 'black'}" style="width: ${100/data.width}%"></div>`;
+            cellHTML += `<div class="cell ${((i + j) % 2 === 0) ? 'white' : 'black'}"></div>`;
         }
         //sugeneruotas celes iterpiame i eilute ir eilute iterpiame prie HTML
-        HTML += `<div class="row" style="height: ${100/data.height}%">${cellHTML}</div>`;
+        HTML += `<div class="row">${cellHTML}</div>`;
     }
     return DOM.innerHTML = HTML;
 
+}
+
+function updateStyles(data) {
+    const style  = `.board > .row {
+                        height: calc(${100/data.height}%);
+                    }
+                        .board > .row > .cell {
+                            width: calc(${100/data.width}%);
+                        }`;
+
+    return document.querySelector(data.styleSelector).innerHTML = style;
 }
 
 // ${j}, ${i}
